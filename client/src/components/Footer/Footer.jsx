baad6c25dec2189e9e1c246eb175f247d49e0186
Nourhan Ebrahim <45333450+abdullahelbadry@users.noreply.github.com>
2022-04-06 14:15:13 +0200

    header component completed

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../css/Footer/Footer.css'
+export default function Footer() {
+    return (
+        <footer className="d-flex justify-content-center align-items-center fw-bold fs-5">
+            Footer
+        </footer>
+    )
+}
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "../../css/Header/Header.css";
+export default function Header() {
+    return (
+        <header className="d-flex justify-content-center align-items-center fw-bold fs-5">
+            Shopping Cart
+        </header>
+    )
+}
