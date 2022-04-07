23ad8005d155608bf854e9aeb789bb34d3985181
Nourhan Ebrahim <45333450+abdullahelbadry@users.noreply.github.com>
2022-04-07 10:59:48 +0200

    footer-component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import '../../css/Footer/Footer.css'
 export default function Footer() {
     return (
         <footer className="d-flex justify-content-center align-items-center fw-bold fs-5">
-            Footer
+            &copy; Copyright Reserved To Yassinco Company 2022
         </footer>
     )
 }
