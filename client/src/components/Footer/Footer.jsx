f2f3c5e264ae73af5410c0ca3bc3b0c88976ea0f
Nourhan Ebrahim <45333450+abdullahelbadry@users.noreply.github.com>
2022-04-07 11:38:52 +0200

    file data created with static words

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../../css/Footer/Footer.css'
+import {words} from "../../words";
 export default function Footer() {
     return (
         <footer className="d-flex justify-content-center align-items-center fw-bold fs-5">
-            &copy; Copyright Reserved To Yassinco Company 2022
+            {words.footerTitle}
         </footer>
     )
 }
---
+++ client/src/words.js
@@ -0,0 +1,5 @@
+export const words =  {
+    headerTitle : 'Shopping Cart',
+    footerTitle : '© CopyRight Reserved To Yassinco Company 2022',
+    content : 'content'
+}
